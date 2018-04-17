d9b44d025ebc13ffa9645422824c15f0415c3082
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-04-17 18:42:42 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,10 @@
 // Code Invitation Component Here
+export deafault class Invitation extends React.Component {
+  redner() {
+    return (
+      <div>
+        <h1>"You've been invited!"</h1>
+      </div>
+    );
+  }
+}
